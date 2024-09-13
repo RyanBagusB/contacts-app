840f237674775744ef15a52fd7800cedf8a201e3
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-13 19:34:30 +0700

    first commit

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ContactItem from './ContactItem';
+
+const ContactList = ({ contacts }) => {
+
+  return (
+    <div className='contact-list'>
+      {
+        contacts.map((contact) => (
+          <ContactItem {...contact} key={contact.id} />
+        ))
+      }
+    </div>
+  );
+};
+
+export default ContactList;
